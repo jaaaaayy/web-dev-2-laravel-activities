61b136ad2cf1db86f2ce4d73b8335bd0dfb59ef4
Jay Vallespin <jayvallespin51@gmail.com>
2025-06-07 16:53:44 +0800

    Activity 5: Add route protection

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,24 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(AuthController::class)->group(function () {
+Route::middleware('guest')->group(function () {
+  Route::controller(AuthController::class)->group(function () {
     Route::get('/', 'index')->name('auth.index');
     Route::post('login', 'login')->name('auth.login');
     Route::get('register', 'create')->name('auth.create');
     Route::post('/', 'register')->name('auth.register');
-    Route::get('/logout', 'logout')->name('auth.logout');
   });
+});
 
-Route::controller(StudentController::class)->group(function () {
+Route::middleware('auth')->group(function () {
+  Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+  Route::controller(StudentController::class)->group(function () {
     Route::get('students', 'index')->name('students.index');
     Route::get('students/create', 'create')->name('students.create');
     Route::post('students', 'store')->name('students.store');
     Route::get('students/{student}/edit', 'edit')->name('students.edit');
     Route::put('students/{student}', 'update')->name('students.update');
     Route::delete('students/{student}', 'destroy')->name('students.destroy');
+  });
 });
